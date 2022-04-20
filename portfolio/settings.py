23638c1b44a408c731a0627376a4c4ff2b52c755
Luis Felipe Souza <56547061+lfcunha07@users.noreply.github.com>
2022-04-20 10:36:50 -0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': '192.168.0.1',
+        'HOST': '192.168.15.0',
         'PORT': '5432',
     }
 }
